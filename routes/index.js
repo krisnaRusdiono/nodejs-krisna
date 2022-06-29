1c624782d95e08cbe8af9270200ad93156737817
krisna <krisna@radyalabs.id>
2022-06-29 16:02:03 +0700

    update return index

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('index', { title: 'Express' });
+  res.send('Hi bos!');
 });
 
 module.exports = router;
